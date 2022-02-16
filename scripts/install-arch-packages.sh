b2d35f331a400f7c67f41163ee8174cc1c29ebda
helmesjo <helmesjo@gmail.com>
2022-02-17 00:23:32 +0100

    Changed arch packages to match i3wm-themer.

--- scripts/install-arch-packages.sh
+++ scripts/install-arch-packages.sh
@@ -3,8 +3,8 @@ set -eu -o pipefail
 
 echo "Installing required packages..."
 
-pacpackages="mesa xorg i3 lightdm-gtk-greeter pulseaudio-alsa noto-fonts noto-fonts-extra noto-fonts-emoji git kitty vim"
-aurpackages="rlaunch siji-git polybar"
+pacpackages="mesa xorg vim git rxvt-unicode alsa-utils lightdm-gtk-greeter i3-gaps nitrogen rofi compton ttf-font-awesome adobe-source-code-pro-fonts ttf-nerd-fonts-symbols"
+aurpackages="polybar"
 
 # Install packages
 sudo pacman -S --noconfirm $pacpackages
