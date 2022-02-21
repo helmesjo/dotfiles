78674705892add4d834031842dcae75fd2248f82
helmesjo <helmesjo@gmail.com>
2022-02-21 20:55:38 +0100

    Replace polybar with i3blocks (no fancy config yet).

--- scripts/install-arch-packages.sh
+++ scripts/install-arch-packages.sh
@@ -3,8 +3,8 @@ set -eu -o pipefail
 
 echo "Installing required packages..."
 
-pacpackages="mesa xorg vim git rxvt-unicode alsa-utils lightdm-gtk-greeter i3-gaps nitrogen rofi compton ttf-font-awesome adobe-source-code-pro-fonts ttf-nerd-fonts-symbols"
-aurpackages="polybar"
+pacpackages="mesa xorg vim git rxvt-unicode alsa-utils lightdm-gtk-greeter i3-gaps i3blocks nitrogen rofi compton ttf-font-awesome adobe-source-code-pro-fonts ttf-nerd-fonts-symbols"
+#aurpackages=""
 
 # Install yay if missing
 if ! command -v yay &> /dev/null; then
@@ -16,7 +16,7 @@ fi
 
 # Install packages
 sudo pacman -S --noconfirm $pacpackages
-yay -S --noconfirm $aurpackages
+#yay -S --noconfirm $aurpackages
 
 # Build git-credential-libsecret
 (cd /usr/share/git/credential/libsecret && sudo make)
