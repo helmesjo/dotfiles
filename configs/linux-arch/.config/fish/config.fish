6bac0078ba8192bdacbfa47aa632df532090e6de
helmesjo <helmesjo@gmail.com>
2022-08-28 19:20:47 +0200

    macos/linux: Change fish config to use 'readlink -f' instead of 'readlink -m' (latter option is not available on macos).

--- configs/linux-arch/.config/fish/config.fish
+++ configs/linux-arch/.config/fish/config.fish
@@ -7,7 +7,7 @@ set -x EDITOR $VISUAL
 
 if status is-interactive
     # Path to file, or source if symlink.
-    set config_path (dirname (readlink -m (status --current-filename)))
+    set config_path (dirname (readlink -f (status --current-filename)))
     set dotfiles_root (git -C $config_path rev-parse --show-toplevel)
     # Sourcing
 
