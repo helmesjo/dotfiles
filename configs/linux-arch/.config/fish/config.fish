b76ca9b0fcdf01b8358263d0cf8b90b47c688430
helmesjo@gmail.com <helmesjo@gmail.com>
2022-08-29 23:01:55 +0200

    linux: Only add 'alias hx=helix' if bin 'helix' exists (less brittle check than previous which just checked 'if arch linux').

--- configs/linux-arch/.config/fish/config.fish
+++ configs/linux-arch/.config/fish/config.fish
@@ -9,25 +9,24 @@ if status is-interactive
     # Path to file, or source if symlink.
     set config_path (dirname (readlink -f (status --current-filename)))
     set dotfiles_root (git -C $config_path rev-parse --show-toplevel)
-    # Sourcing
-
-    # Export
-    set -x FZF_DEFAULT_COMMAND rg --files --hidden
-    set -x FZF_CTRL_T_COMMAND $FZF_DEFAULT_COMMAND
-    set -x FZF_COMPLETION_TRIGGER ??
+    
+    # PATH
+    switch (uname)
+        case Darwin
+            fish_add_path /opt/homebrew/bin
+    end
 
     # Aliases
     alias cat='bat'
     alias config='git -C $dotfiles_root'
     alias ls='ls --color=auto'
     # helix doesn't have a 'hx' bin on arch
-    switch (uname)
-        case Linux
-            set distro (lsb_release -a | awk -F':' '/Distributor ID/{print $2}' | awk '{$1=$1};1')
-            if string match -q 'Arch*' -- "$distro"
-              alias hx='helix'
-            end
-        case Darwin
-            fish_add_path /opt/homebrew/bin
+    if command -v helix &> /dev/null
+        alias hx='helix'
     end
+
+    # Export
+    set -x FZF_DEFAULT_COMMAND rg --files --hidden
+    set -x FZF_CTRL_T_COMMAND $FZF_DEFAULT_COMMAND
+    set -x FZF_COMPLETION_TRIGGER ??
 end
