8200b1ebc7a90bc44810c8e4c49b257b5c9696e4
helmesjo <helmesjo@gmail.com>
2022-12-16 23:41:04 +0100

    macos/fish: Prepend homebrew bin path instead of append (eg. so brew installed bash is picked up instead of system bash).

--- configs/linux-arch/.config/fish/config.fish
+++ configs/linux-arch/.config/fish/config.fish
@@ -13,7 +13,7 @@ if status is-interactive
     # PATH
     switch (uname)
         case Darwin
-            fish_add_path -aP /opt/homebrew/bin
+            fish_add_path -pP /opt/homebrew/bin
             fish_add_path -aP /opt/homebrew/Cellar/llvm/14.0.6_1/bin # lldb-vscode
     end
 
