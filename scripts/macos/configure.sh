73dcf778b1be4826db649396d0a2962509045144
helmesjo <helmesjo@gmail.com>
2023-01-22 23:37:54 +0100

    macos: Set keyboard repeat delay & speed to be faster than what's allowed in the System Settings app.

--- scripts/macos/configure.sh
+++ scripts/macos/configure.sh
@@ -4,3 +4,9 @@ set -eu -o pipefail
 this_dir=$(dirname $(readlink -f $BASH_SOURCE))
 
 $this_dir/configure-fzf.sh
+
+# Some system defaults
+
+## Keyboard repeat delay & speed
+defaults write -g InitialKeyRepeat -int 10 # normal minimum is 15 (225 ms)
+defaults write -g KeyRepeat -int 1 # normal minimum is 2 (30 ms)
