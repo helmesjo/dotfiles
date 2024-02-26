164f7fc752d6f80496a15a95a7d9db4059871614
helmesjo <helmesjo@gmail.com>
2024-02-26 12:51:00 +0100

    windows: Temporarily switch back to use 'fish' instead of nushell, waiting for: https://github.com/nushell/nushell/issues/11983

--- configs/windows/appdata/shortcuts/msys2_shell.cmd
+++ configs/windows/appdata/shortcuts/msys2_shell.cmd
@@ -1,3 +1,4 @@
 @echo off
 
-C:\msys64\msys2_shell.cmd -defterm -here -no-start -ucrt64 -use-full-path -shell bash -c "nu --login --interactive"
+C:\msys64\msys2_shell.cmd -defterm -here -no-start -ucrt64 -use-full-path -shell fish
+rem -c "nu --login --interactive"
