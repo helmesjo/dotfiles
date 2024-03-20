efb9db6286e634dc2b4818929c245fbbe20fd134
helmesjo <helmesjo@gmail.com>
2024-03-20 15:55:19 +0100

    windows: Removed typo and changed so configure-msys.sh runs itself as admin if necessary.

--- scripts/windows/configure-msys.sh
+++ scripts/windows/configure-msys.sh
@@ -1,24 +1,29 @@
 #!/usr/bin/env bash
 set -eu -o pipefail
 
-# Must run with correct HOME dir set
-if [[ $HOME != $(cygpath -u "$USERPROFILE") ]]; then
+# Must run with correct HOME dir set and as admin
+if [[ $HOME != $(cygpath -u "$USERPROFILE") ]] || ! net session > /dev/null 2>&1; then
   # Must run with correct home directory,
   # else it'll create it's own within msys2.
   MSYS=winsymlinks:nativestrict
   HOME=$(cygpath -u "$USERPROFILE")
   this_script=$(readlink -f $BASH_SOURCE)
 
+  # Msys: Deal with '/' being parsed as path & not cmd flag
+  case "$(uname -o)" in
+      Msys) CMD_EXE=(cmd //C);;
+      *)    CMD_EXE=(cmd /C);;
+  esac
+  echo "Re-running as admin with HOME=$HOME"
   test -f "C:/msys64/msys2_shell.cmd"
-  echo "Re-running with HOME=$HOME"
-  cmd.exe //C "C:/msys64/msys2_shell.cmd \
-    -defterm -here -no-start -ucrt64 -shell bash -c $this_script"
+  "$(cygpath -u "$PROGRAMFILES/gsudo/Current/gsudo")" \
+    ${CMD_EXE[@]} C:/msys64/msys2_shell.cmd -defterm -here -no-start -ucrt64 -shell \
+                  bash -c $this_script
   exit 0
 fi
 
 # NOTE: Below are some speed-up tricks found online.
 #       It avoids querying Windows for user details.
-exit
 
 # Create local account details to avoid msys querying
 # from windows each time. Run 'trace ls' for details
