402ef5c6744c230f8d5ee05b895ea306b21379fd
helmesjo <helmesjo@gmail.com>
2024-03-20 18:29:11 +0100

    windows: Change alacritty config to directly start msys2_shell.cmd instead of first starting cmd. Fix shortcut etc.

--- configs/windows/appdata/shortcuts/msys2_shell.cmd
+++ configs/windows/appdata/shortcuts/msys2_shell.cmd
@@ -1,10 +1,8 @@
 @echo off
 
-set MSYS=winsymlinks:nativestrict
-set MSYS2_PATH_TYPE=inherit
-set MSYSTEM=UCRT64
 rem HOME is required else MSYS creates its own /home/<user> dir which messes things up.
 rem It needs to be converted to forward-slashes.
 set "HOME=%USERPROFILE%"
 set "HOME=%HOME:\=/%"
-C:\msys64\msys2_shell.cmd -ucrt64 -defterm -here -no-start -use-full-path -shell bash -c "source ~/.vsdevenv.sh && fish"
+
+C:\msys64\msys2_shell.cmd %*
