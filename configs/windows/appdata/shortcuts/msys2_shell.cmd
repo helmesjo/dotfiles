734c2423d9e756481f0770340d11de1818108f87
helmesjo <helmesjo@gmail.com>
2024-03-21 01:25:46 +0100

    windows: Enable 'winsymlinks=native' so that git clone with submodules and symlinks to these work as expected with msys git.

--- configs/windows/appdata/shortcuts/msys2_shell.cmd
+++ configs/windows/appdata/shortcuts/msys2_shell.cmd
@@ -2,6 +2,7 @@
 
 rem HOME is required else MSYS creates its own /home/<user> dir which messes things up.
 rem It needs to be converted to forward-slashes.
+set MSYS=winsymlinks:native
 set "HOME=%USERPROFILE%"
 set "HOME=%HOME:\=/%"
 
