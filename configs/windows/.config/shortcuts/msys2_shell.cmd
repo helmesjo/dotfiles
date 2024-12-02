f56b859ed99c087ca148e8577bd953a5bf408e9e
helmesjo <helmesjo@gmail.com>
2024-12-02 13:35:48 +0100

    windows: Add error checking when starting shell so that it doesn't instantly close the terminal on error.

--- configs/windows/.config/shortcuts/msys2_shell.cmd
+++ configs/windows/.config/shortcuts/msys2_shell.cmd
@@ -6,4 +6,14 @@ set MSYS=winsymlinks:native
 set "HOME=%USERPROFILE%"
 set "HOME=%HOME:\=/%"
 
-C:\msys64\msys2_shell.cmd %*
+IF EXIST "C:\msys64\msys2_shell.cmd" (
+  cmd /C "C:\msys64\msys2_shell.cmd %*"
+  IF ERRORLEVEL 1 (
+    ECHO Failed to start shell >&2
+    cmd.exe
+  )
+) ELSE (
+    ECHO File C:\msys64\msys2_shell.cmd does not exist, install Msys2: winget install Msys2
+    cmd.exe
+    exit 1
+)
