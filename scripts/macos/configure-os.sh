ceb7441a8cdcdb9a0d7f57e477bbca28c219becb
helmesjo <helmesjo@gmail.com>
2024-12-20 10:15:29 +0100

    macos: Disable 'press & hold' pallete. It makes moving cursor in eg. vscode (helix/vim plugin) not function.

--- scripts/macos/configure-os.sh
+++ scripts/macos/configure-os.sh
@@ -3,5 +3,6 @@ set -eu -o pipefail
 unalias -a # disable aliases for script
 
 ## Keyboard repeat delay & speed
-defaults write -g InitialKeyRepeat -int 10 # normal minimum is 15 (225 ms)
-defaults write -g KeyRepeat -int 1 # normal minimum is 2 (30 ms)
+defaults write -g InitialKeyRepeat -int 10             # normal minimum is 15 (225 ms)
+defaults write -g KeyRepeat -int 1                     # normal minimum is 2 (30 ms)
+defaults write -g ApplePressAndHoldEnabled -bool false # disable "press & hold" pallete
