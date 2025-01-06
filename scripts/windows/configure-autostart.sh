67ebb00e160e28332e9b8ed8423ff5b5534519f8
helmesjo <helmesjo@gmail.com>
2025-01-06 17:26:51 +0100

    configure-autostart.sh: Handle whitespace in filenames.

--- scripts/windows/configure-autostart.sh
+++ scripts/windows/configure-autostart.sh
@@ -4,28 +4,22 @@ unalias -a # disable aliases for script
 
 export MSYS=winsymlinks:nativestrict
 
-# Symlinks for shortcuts requires elevated privileges.
-if ! net session > /dev/null 2>&1; then
-  echo "Re-run as admin" >&2
-  exit 1
-fi
-
 this_dir=$(cygpath -u `dirname $(readlink -f $BASH_SOURCE)`)
 root_dir=$(cygpath -u $(git -C "$(cygpath -m $this_dir)" rev-parse --show-toplevel))
 os=$($root_dir/scripts/get-os.sh 2>&1)
 dotfiles_root="$root_dir/configs/$os"
 startup_root="$dotfiles_root/.config/startup"
-startup=$(ls $startup_root) # grab the list
 
 # Copy each shortcut into Windows Startup directory
 # so that they autostart upon login.
-for sourcename in ${startup[@]}; do
+for sourcename in "$startup_root"/*; do
   # get absolute path
+  sourcename="${sourcename##*/}"
   sourcepath="$startup_root/$sourcename"
   targetpath="$APPDATA/Microsoft/Windows/Start Menu/Programs/Startup/$sourcename"
 
   # Skip untracked files
-  if [ -z "$(git -C "$(cygpath -m $startup_root)" ls-files $sourcename)" ]; then
+  if [ -z "$(git -C "$(cygpath -m $startup_root)" ls-files "$sourcename")" ]; then
     echo "  - Skipping untracked '$sourcename'"
     continue
   fi
