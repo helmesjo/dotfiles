e86f29352a662b93c157dfac2c07239e129b29b6
helmesjo <helmesjo@gmail.com>
2025-01-10 00:08:01 +0100

    komorebi: Stop and restart service(s) during configure.

--- scripts/windows/configure-komorebi.sh
+++ scripts/windows/configure-komorebi.sh
@@ -1,2 +1,5 @@
 setx KOMOREBI_CONFIG_HOME "$(cygpath -m "$HOME/.config/komorebi/")"
 setx WHKD_CONFIG_HOME "$(cygpath -m "$HOME/.config/whkd/")"
+
+komorebic stop --whkd --bar
+komorebic start --whkd --bar
