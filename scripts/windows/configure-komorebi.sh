467aa13c82189cef4cd9ee51e8673ef3b4252da3
helmesjo <helmesjo@gmail.com>
2025-05-28 14:49:58 +0200

    windows: Don't fail if 'komorebi stop' fails (because it may very well not be running).

--- scripts/windows/configure-komorebi.sh
+++ scripts/windows/configure-komorebi.sh
@@ -5,5 +5,5 @@ unalias -a # disable aliases for script
 setx KOMOREBI_CONFIG_HOME "$(cygpath -m "$HOME/.config/komorebi")"
 setx WHKD_CONFIG_HOME "$(cygpath -m "$HOME/.config/whkd")"
 
-komorebic stop --whkd --bar
+komorebic stop --whkd --bar >/dev/null 2>&1 || true
 komorebic start --whkd --bar
