4a50381af5abf571f50b445f2e367871ed9902be
helmesjo <helmesjo@gmail.com>
2025-08-22 10:14:13 +0200

    windows: Enable Msys2 native symlinks in root setup.sh to make sure it gets enabled (eg. when run in git-bash).

--- setup.sh
+++ setup.sh
@@ -7,6 +7,10 @@ os=$($root_dir/scripts/get-os.sh 2>&1)
 
 echo "Setting up configuration for OS '$os'..."
 
+if [[ $os == windows ]]; then
+  export MSYS=winsymlinks:nativestrict
+fi
+
 # install packages for os
 $root_dir/scripts/$os/install.sh
 # configure os
