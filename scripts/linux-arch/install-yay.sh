8f734641afe33d7e8ea910a4f9b217eee780779a
helmesjo <helmesjo@gmail.com>
2025-11-19 16:08:31 +0100

    linux: Make sure 'base-devel' is installed (required to build yay).

--- scripts/linux-arch/install-yay.sh
+++ scripts/linux-arch/install-yay.sh
@@ -7,7 +7,9 @@ BRANCH=
 NAME=$(basename $URL)
 DIR="/tmp/build/$NAME"
 if ! command -v yay >/dev/null 2>&1; then
+  sudo pacman -S --needed base-devel
   echo "Installing $NAME $BRANCH to $DIR..."
+  rm -rf "$DIR/"
   mkdir -p "$DIR"
   git clone --quiet --depth=1 https://aur.archlinux.org/yay $DIR >/dev/null
   (cd $DIR && makepkg -Acs --noconfirm)
