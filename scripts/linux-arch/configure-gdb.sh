eaebb8c9a2f92ab3a8c4351dfec1f3db8084994b
helmesjo <helmesjo@gmail.com>
2026-01-13 15:57:23 +0100

    linux: Silence 'tee'.

--- scripts/linux-arch/configure-gdb.sh
+++ scripts/linux-arch/configure-gdb.sh
@@ -19,4 +19,4 @@ set -eu -o pipefail
 #  SSH connection via a client like putty.exe in windows.
 #
 # See: https://github.com/Microsoft/MIEngine/wiki/Troubleshoot-attaching-to-processes-using-GDB
-echo 0 | sudo tee /proc/sys/kernel/yama/ptrace_scope
+echo 0 | sudo tee /proc/sys/kernel/yama/ptrace_scope >/dev/null
