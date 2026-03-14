ddf0a2ac935842b3b8ee89cfd87d6c34e90a783d
helmesjo <helmesjo@gmail.com>
2026-03-14 12:24:50 +0100

    linux: Install yay-bin (prebuilt) instead of building from source.

--- scripts/linux-arch/install-yay.sh
+++ scripts/linux-arch/install-yay.sh
@@ -2,17 +2,16 @@
 
 set -eu -o pipefail
 
-URL="https://aur.archlinux.org/yay"
-BRANCH=
-NAME=$(basename $URL)
-DIR="/tmp/build/$NAME"
 if ! command -v yay >/dev/null 2>&1; then
+  URL="https://aur.archlinux.org/yay-bin.git"
+  BRANCH=
+  NAME=$(basename $URL)
+  DIR="$(mktemp -d)"
+
   sudo pacman -S --needed base-devel
-  echo "Installing $NAME $BRANCH to $DIR..."
-  rm -rf "$DIR/"
-  mkdir -p "$DIR"
-  git clone --quiet --depth=1 https://aur.archlinux.org/yay $DIR >/dev/null
-  (cd $DIR && makepkg -Acs --noconfirm)
-  sudo pacman -U --noconfirm $DIR/*.pkg.tar.zst
+  echo "Installing $NAME $BRANCH (temp dir: $DIR)..."
+  sudo pacman -S --needed git base-devel
+  git clone --quiet --depth=1 $URL $DIR >/dev/null
+  (cd $DIR && makepkg -si --noconfirm)
   rm -rf $DIR
 fi
