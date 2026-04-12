819a8419a5b2eaa2ba654410052c80d56da0aa7a
helmesjo <helmesjo@gmail.com>
2026-04-12 20:41:02 +0200

    zen: Disable zen/firefox autofill (use bitwarden instead).

--- configs/linux-arch/.config/zen/13pmdhsh.fho/user.js
+++ configs/linux-arch/.config/zen/13pmdhsh.fho/user.js
@@ -1,3 +1,10 @@
 user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
 user_pref("zen.welcome-screen.seen", true);
 user_pref("zen.welcomeScreen.seen", true);
+// Disable Firefox native password manager & autofill (use Bitwarden)
+user_pref("signon.rememberSignons", false);
+user_pref("signon.autofillForms", false);
+user_pref("signon.autofillForms.http", false);
+user_pref("browser.formfill.enable", false);
+user_pref("extensions.formautofill.addresses.enabled", false);
+user_pref("extensions.formautofill.creditCards.enabled", false);
