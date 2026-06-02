11f710a1388445941ea18d664df53166b5d64472
helmesjo <helmesjo@gmail.com>
2026-06-02 18:55:15 +0200

    zsh,bash: bind Alt+Backspace to stop at special characters

--- configs/linux-arch/.bashrc.d/25-keybindings.bash
+++ configs/linux-arch/.bashrc.d/25-keybindings.bash
@@ -8,3 +8,24 @@ _bash_shift_enter() {
   READLINE_POINT=$((READLINE_POINT + 1))
 }
 bind -x '"\e[13;2u": _bash_shift_enter'
+
+# Alt+Backspace: kill one token at a time, stopping at special characters.
+# Readline has no word-char setting, so this is implemented as a shell function.
+_bash_backward_kill_to_special() {
+  local i=$((READLINE_POINT - 1))
+  [[ $i -lt 0 ]] && return
+  # Skip trailing spaces (mimic standard backward-kill-word space-skipping)
+  while [[ $i -ge 0 && "${READLINE_LINE:$i:1}" == ' ' ]]; do ((i--)); done
+  [[ $i -lt 0 ]] && { READLINE_POINT=0; READLINE_LINE="${READLINE_LINE:$READLINE_POINT}"; return; }
+  local char="${READLINE_LINE:$i:1}"
+  if [[ "$char" =~ [[:alnum:]_] ]]; then
+    while [[ $i -ge 0 && "${READLINE_LINE:$i:1}" =~ [[:alnum:]_] ]]; do ((i--)); done
+  else
+    while [[ $i -ge 0 && ! "${READLINE_LINE:$i:1}" =~ [[:alnum:]_[:space:]] ]]; do ((i--)); done
+  fi
+  local new_pt=$((i + 1))
+  READLINE_LINE="${READLINE_LINE:0:$new_pt}${READLINE_LINE:$READLINE_POINT}"
+  READLINE_POINT=$new_pt
+}
+bind -x '"\e\177": _bash_backward_kill_to_special'  # alt+backspace (ESC+DEL, most terminals)
+bind -x '"\e\010": _bash_backward_kill_to_special'  # alt+backspace (ESC+BS, some terminals)
--- configs/linux-arch/.zsh.d/25-keybindings.zsh
+++ configs/linux-arch/.zsh.d/25-keybindings.zsh
@@ -2,6 +2,24 @@ bindkey '^[[1;3C' forward-word                   # alt+right
 bindkey '^[[1;3D' backward-word                  # alt+left
 bindkey '^[[3~'   delete-char                    # 'Del' key
 
+# Alt+Backspace: kill one token at a time, stopping at special characters.
+# Uses %% (right-to-left C-level glob) so cost is O(token length), not O(buffer size).
+# Strips trailing spaces first, then kills one run of word chars or special chars.
+_zle_backward_kill_to_special() {
+    setopt localoptions extendedglob
+    local stripped="${LBUFFER%%[[:space:]]##}"
+    if [[ -z $stripped ]]; then
+        LBUFFER=''
+    elif [[ $stripped = *[[:alnum:]_] ]]; then
+        LBUFFER="${stripped%%[[:alnum:]_]##}"
+    else
+        LBUFFER="${stripped%%[^[:alnum:][:space:]_]##}"
+    fi
+}
+zle -N _zle_backward_kill_to_special
+bindkey '\e^?' _zle_backward_kill_to_special  # alt+backspace (ESC+DEL, most terminals)
+bindkey '\e^H' _zle_backward_kill_to_special  # alt+backspace (ESC+BS, some terminals)
+
 # Shift+Enter: insert a literal newline into the ZLE buffer (no execute, no dquote prompt)
 _zle_shift_enter() { LBUFFER+=$'\n' }
 zle -N _zle_shift_enter
